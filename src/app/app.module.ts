48db742c757237ac937ed65fa8fb683cc98717d7
Nermine Ben Jemaa <nerminbenjemaa@gmail.com>
2021-02-09 14:13:05 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterSimulatorComponent } from './components/router-simulator/router-simulator.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
+import { ThirdComponent } from './components/third/third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouterSimulatorComponent,
+    HeaderComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule,
